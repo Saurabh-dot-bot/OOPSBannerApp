b415690a56bbbfbe938b9231b30c16eb00a8ea78
Saurabh Kumar <sk0130@srmist.edu.in>
2026-03-22 21:12:45 +0530

    Hello App

---
+++ HelloApp4.java
@@ -0,0 +1,15 @@
+public class HelloApp4 {
+
+    public static void main(String[] args) {
+
+        String result;
+
+        if (args.length > 0) {
+            result = String.join(", ", args);
+        } else {
+            result = "World";
+        }
+
+        System.out.println("Hello, " + result + "!");
+    }
+}
