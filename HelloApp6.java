52fd0f45de4bc4d9e9fdef932c25792a53bbf362
Saurabh Kumar <sk0130@srmist.edu.in>
2026-03-22 21:34:56 +0530

    HelloApp6

---
+++ HelloApp6.java
@@ -0,0 +1,22 @@
+public class HelloApp6 {
+
+    public static void main(String[] args) {
+
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+
+            StringBuilder nameBuilder = new StringBuilder();
+
+            // Append all names with ", "
+            for (String name : args) {
+                nameBuilder.append(name).append(", ");
+            }
+
+            // Remove trailing ", "
+            String names = nameBuilder.substring(0, nameBuilder.length() - 2);
+
+            System.out.println("Hello, " + names + "!");
+        }
+    }
+}
