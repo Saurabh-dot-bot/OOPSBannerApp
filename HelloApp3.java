756c312d68914aa1d536cf0739f6a8256c980124
Saurabh Kumar <sk0130@srmist.edu.in>
2026-03-22 20:55:07 +0530

    HelloApp

---
+++ HelloApp3.java
@@ -0,0 +1,11 @@
+public class HelloApp3 {
+
+    public static void main(String[] args) {
+
+        // Assign name using ternary operator
+        String name = (args.length > 0) ? args[0] : "World";
+
+        // Display greeting
+        System.out.println("Hello, " + name + "!");
+    }
+}
